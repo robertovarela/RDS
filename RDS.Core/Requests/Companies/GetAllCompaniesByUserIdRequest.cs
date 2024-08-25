6675d6608b6492ade6fce6caaffd45400d6c0006
robertovarela <roberto.rn@gmail.com>
2024-08-25 14:56:08 -0300

    Rename GetAllComaniesByUserIdRequest to correct typo
    Corrected the typo in the filename and class name from "GetAllComaniesByUserIdRequest" to "GetAllCompaniesByUserIdRequest". Also made a minor class attribute adjustment in ConfigurationGroup.razor to enhance styling consistency.

--- RDS.Core/Requests/Companies/GetAllCompaniesByUserIdRequest.cs
+++ RDS.Core/Requests/Companies/GetAllCompaniesByUserIdRequest.cs
@@ -1,6 +1,6 @@
 ﻿namespace RDS.Core.Requests.Companies;
 
-public class GetAllComaniesByUserIdRequest : PagedRequest
+public class GetAllCompaniesByUserIdRequest : PagedRequest
 {
     public long UserId { get; set; }
 }
