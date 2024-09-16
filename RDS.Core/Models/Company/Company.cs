2a8df23609b07c4900972b4020a75c8ea6421155
robertovarela <roberto.rn@gmail.com>
2024-09-15 23:34:46 -0300

    Add CompanyRequestUser model and clean up Company.cs
    Introduce a new model, CompanyRequestUser, to handle user requests for companies. Additionally, remove extraneous comments from the OwnerId property in the Company model for better code clarity.

--- RDS.Core/Models/Company/Company.cs
+++ RDS.Core/Models/Company/Company.cs
@@ -5,7 +5,7 @@ public class Company
     public long Id { get; set; }
     public string Name { get; set; } = null!;
     public string Description { get; set; } = null!;
-    public long OwnerId { get; set; } // Proprietário da empresa
+    public long OwnerId { get; set; }
     public virtual ApplicationUser.ApplicationUser Owner { get; set; } = null!; // Propriedade de navegação
 
     public virtual ICollection<CompanyUser> CompanyUsers { get; set; } = new List<CompanyUser>();
---
+++ RDS.Core/Models/Company/CompanyRequestUser.cs
@@ -0,0 +1,13 @@
+﻿namespace RDS.Core.Models.Company;
+
+public class CompanyRequestUser
+{
+    public long Id { get; set; }
+    public long CompanyId { get; set; }
+    public long OwnerId { get; set; }
+    public string CompanyName { get; set; } = null!;
+    public string ConfirmationCode { get; set; } = null!;
+    public DateTime ExpirationDate { get; set; }
+    public DateTime ConfirmationDate { get; set; }
+    
+}
